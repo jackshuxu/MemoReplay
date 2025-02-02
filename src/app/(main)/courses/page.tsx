5ba281e6b675c51810d4444a7f94060ba52a22b9
jackshuxu <jackxushu77@gmail.com>
2025-02-02 00:27:52 -0500

    typo

--- src/app/(main)/courses/page.tsx
+++ src/app/(main)/courses/page.tsx
@@ -20,7 +20,7 @@ const CoursesPage = async () => {
   return (
     <div className="mx-auto h-full max-w-[912px] px-3">
       <h1 className="text-2xl font-bold text-neutral-700">Language Courses</h1>
-      <List courses={courses} activreCourseId={userProgress?.activeCourseId} />
+      <List courses={courses} activeCourseId={userProgress?.activeCourseId} />
     </div>
   );
 };
